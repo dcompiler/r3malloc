4a7eb02f885cced01274f92bbacbeebedfe45e20
Paul <ppastary@u.rochester.edu>
2022-02-12 19:32:58 -0800

    Change Anchor avail, count field size dependencies
    They now depend on lg_max_block_num instead of being const 31

--- src/heap.rs
+++ src/heap.rs
@@ -1,4 +1,16 @@
+#[inline(always)]
+const fn lg_max_block_num() -> u32 {
+	31
+}
+
+#[inline(always)]
+pub const fn max_block_num() -> u32 {
+	2 << lg_max_block_num()
+}
 pub struct Anchor {
+	// state is first 2 bits
+	// avail is next lg_max_block_num (31) bits
+	// count is next lg_max_block_num (31) bits
 	anch: u64
 }
 
@@ -8,19 +20,19 @@ impl Anchor {
 	}
 
 	pub fn set_state(&mut self, state: u32) {
-		self.anch = (self.anch & 0x3FFFFFFFFFFFFFFF) | ((state as u64) << 62)
+		self.anch = (self.anch & 0x3FFFFFFFFFFFFFFF) | ((state as u64) << 2 * lg_max_block_num())
 	}
 
 	pub fn get_state(&self) -> u32 {
-		(self.anch >> 62) as u32
+		(self.anch >> 2 * lg_max_block_num()) as u32
 	}
 
 	pub fn set_avail(&mut self, avail: u32) {
-		self.anch = (self.anch & 0xC00000007FFFFFFF) | ((avail as u64) << 30)
+		self.anch = (self.anch & 0xC00000007FFFFFFF) | ((avail as u64) << lg_max_block_num() - 1)
 	}
 
 	pub fn get_avail(&self) -> u32 {
-		((self.anch >> 31) & 0x007FFFFFF) as u32
+		((self.anch >> lg_max_block_num()) & 0x007FFFFFF) as u32
 	}
 
 	pub fn set_count(&mut self, count: u32) {
