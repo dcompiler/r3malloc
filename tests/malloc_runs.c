6db7aac4423198fb056b80d950e9da81536f9f63
Paul <ppastary@u.rochester.edu>
2022-02-26 23:20:23 -0800

    Add small malloc test

---
+++ tests/malloc_runs.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void *malloc(long unsigned int size);
+
+int main() {
+	int *arr = (int*)malloc(10 * sizeof(int));
+	for (int i = 0; i < 10; i++)
+		arr[i] = i + 1;
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", arr[i]);
+}
