6c5903b6dc7f0cd20493cb29d4ae8fc43fea6f7e
Paul <ppastary@u.rochester.edu>
2022-03-02 13:00:18 -0800

    Fix arithmetic bugs in anchor state calculation

--- src/heap.rs
+++ src/heap.rs
@@ -48,11 +48,11 @@ impl Anchor {
     }
 
     pub fn set_avail(&mut self, avail: u32) {
-        self.anch = (self.anch & 0xC00000007FFFFFFF) | ((avail as u64) << LG_MAX_BLOCK_NUM - 1)
+        self.anch = (self.anch & 0xC00000007FFFFFFF) | ((avail as u64) << LG_MAX_BLOCK_NUM)
     }
 
     pub fn get_avail(&self) -> u32 {
-        ((self.anch >> LG_MAX_BLOCK_NUM) & 0x007FFFFFF) as u32
+        ((self.anch >> LG_MAX_BLOCK_NUM) & 0x07FFFFFFF) as u32
     }
 
     pub fn set_count(&mut self, count: u32) {
