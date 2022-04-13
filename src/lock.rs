0ef2e4ea818a0396d82fb501082359796f6eb1c0
Paul <ppastary@u.rochester.edu>
2022-04-13 00:36:55 -0700

    Fix acquire/release of Mutex, added fencing

--- src/lock.rs
+++ src/lock.rs
@@ -1,30 +1,21 @@
-// Credit: https://gist.github.com/tstellanova/9b8ec9f1a6d4d928931d171c7b3b914a
-use core::sync::atomic::{AtomicBool, Ordering};
-use core::hint::spin_loop;
+use core::sync::atomic::{AtomicBool, Ordering, fence};
 
 pub struct Mutex {
     lock: AtomicBool,
 }
   
 impl Mutex {
+	pub const fn new() -> Self {
+		Mutex { lock: AtomicBool::new(false) }
+	}
+
     pub fn acquire(&mut self) {
-    	loop {
-	        match self.lock.compare_exchange_weak(false, true, Ordering::Acquire, Ordering::Acquire) {
-	        	Ok(_) => {
-	        		while self.lock.load(Ordering::Relaxed) {
-		                spin_loop();
-		            }
-	        	},
-	        	Err(_) => (),
-	        }
-	    }
+	    while self.lock.compare_exchange_weak(false, true, Ordering::Relaxed, Ordering::Relaxed).is_err() {}
+	    fence(Ordering::Acquire);
     }
 
     pub fn release(&mut self) {
-        match self.lock.compare_exchange_weak(true, false, Ordering::Acquire, Ordering::Acquire) {
-        	Ok(_) => (),
-        	Err(_) => (),
-        }
+        self.lock.store(false, Ordering::Release)
     }
   
 }
