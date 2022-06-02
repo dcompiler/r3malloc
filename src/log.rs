f908f9efdca9b8b3c7a808986d898fa845afa3a8
Paul <ppastary@u.rochester.edu>
2022-06-01 18:21:04 -0700

    Add save_log to save logging information to a file

--- src/log.rs
+++ src/log.rs
@@ -1,8 +1,24 @@
+use libc::c_char;
+use crate::defines::parse_usize;
+
 // Credit to https://stackoverflow.com/questions/38088067/equivalent-of-func-or-function-in-rust
 pub const LOG: bool = match option_env!("LOG") {
     Some(_) => true,
     None => false,
 };
+pub const SAVE_LOG: bool = match option_env!("SAVE_LOG") {
+    Some(_) => true,
+    None => false,
+};
+// by default, save on every invocation of save_log
+pub const SAVE_PERIOD: usize = match option_env!("SAVE_PERIOD") {
+    Some(sp) => parse_usize(sp),
+    None => 0,
+};
+// would be nice to make it compile-time inputted (as above)
+// but I cannot figure out how to null-terminate the input &str
+pub const FILE_PATH: &str = "log.txt\0";
+static mut CURR_PERIOD: usize = 0;
 
 #[macro_export]
 macro_rules! function {
@@ -51,4 +67,32 @@ macro_rules! log_err {
             libc_eprintln!();
         }
     }};
+}
+
+#[inline(never)]
+pub fn save_log(log: &str) {
+    if !SAVE_LOG {
+        return;
+    }
+
+    unsafe {
+        if SAVE_PERIOD != 0 {
+            if CURR_PERIOD != SAVE_PERIOD {
+                CURR_PERIOD += 1;
+                return;
+            } else {
+                CURR_PERIOD = 0;
+            }
+        }
+
+        let fp = FILE_PATH.as_ptr().cast::<core::ffi::c_void>() as *mut c_char;
+        let fd = libc::open(fp, libc::O_WRONLY | libc::O_CREAT | libc::O_APPEND, libc::S_IWRITE | libc::S_IREAD);
+        if fd < 0 {
+            panic!("Failed to open log file.");
+        }
+
+        libc::write(fd,log.as_ptr().cast::<core::ffi::c_void>(), log.len());
+
+        libc::close(fd);
+    }
 }
